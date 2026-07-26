28a600c6ebb67578b42ca15ff3f123668a0b9f16
Sebastian <Sebastian.Mierzwinski@kombatanci.gov.pl>
2026-07-26 10:58:38 +0200

    Roshar: dogranie zaległych fixów (JEI, Lithostitched, Freecam, DH, cleanup)
    - JEI zaktualizowany do 19.39.0.371 (wymagane przez Sophisticated Core)
    - Lithostitched: side = both (wymagane przez Tectonic)
    - Freecam: side = client (crashował dedykowany serwer)
    - Usunięto Foxy's (branded content, crash na macOS)
    - Usunięto branded content: trial_pops.js, blok justcreatepop w tooltips.js
    - Naprawiono tytuł okna gry w kubejs/config/client.json
    - Dodano Distant Horizons

--- neoforge/roshar/kubejs/server_scripts/tooltips.js
+++ neoforge/roshar/kubejs/server_scripts/tooltips.js
@@ -1,19 +1,8 @@
 ItemEvents.modifyTooltips(event => {
-    
     event.add(
-
-        '@justcreatepop',
-        [
-        '§7Found In Trial Vaults§r'
-        ]
-    );
-
-    event.add(
-
         'create:blaze_burner',
         [
         '§6Now Accepting Fluid Lava§r'
         ]
     );
-
 });
